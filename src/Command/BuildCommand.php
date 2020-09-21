a61288d1c794ebb55f8192f173a2f20c32406324
Thomas Jarrand <thomas.jarrand@elao.com>
2020-09-21 10:44:35 +0200

    Show second precision

--- src/Command/BuildCommand.php
+++ src/Command/BuildCommand.php
@@ -149,13 +149,46 @@ class BuildCommand extends Command
         return Command::SUCCESS;
     }
 
+    public static function formatTimePrecision($secs)
+    {
+        static $timeFormats = [
+            [1, 'sec', 1, 2],
+            [2, 'secs', 1, 2],
+            [60, '1 min'],
+            [120, 'mins', 60],
+            [3600, '1 hr'],
+            [7200, 'hrs', 3600],
+            [86400, '1 day'],
+            [172800, 'days', 86400],
+        ];
+
+        foreach ($timeFormats as $index => $format) {
+            if ($secs >= $format[0]) {
+                if ((isset($timeFormats[$index + 1]) && $secs < $timeFormats[$index + 1][0])
+                    || $index == \count($timeFormats) - 1
+                ) {
+                    switch (\count($format)) {
+                        case 2:
+                            return $format[1];
+
+                        case 4:
+                            return round($secs / $format[2], $format[3]) . ' ' . $format[1];
+
+                        default:
+                            return floor($secs / $format[2]) . ' ' . $format[1];
+                    }
+                }
+            }
+        }
+    }
+
     public static function formatEvent(StopwatchEvent $event): string
     {
         return sprintf(
             'Start time: %s — End time: %s — Duration: %s — Memory used: %s',
             date('H:i:s', ($event->getOrigin() + $event->getStartTime()) / 1000),
             date('H:i:s', ($event->getOrigin() + $event->getEndTime()) / 1000),
-            Helper::formatTime($event->getDuration() / 1000),
+            static::formatTimePrecision($event->getDuration() / 1000),
             Helper::formatMemory($event->getMemory())
         );
     }
