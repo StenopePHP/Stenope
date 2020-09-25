57ce41475116c66a73903ef737ef7fe3590a7fd6
Thomas Jarrand <thomas.jarrand@elao.com>
2020-09-25 15:40:19 +0200

    Added utility for handlign contents

---
+++ src/Service/ContentUtils.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of the "Tom32i/Content" bundle.
+ *
+ * @author Thomas Jarrand <thomas.jarrand@gmail.com>
+ */
+
+namespace Content\Service;
+
+use Symfony\Component\PropertyAccess\PropertyAccess;
+use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
+
+class ContentUtils
+{
+    private static ?PropertyAccessorInterface $propertyAccessor = null;
+
+    /**
+     * Get max value of the given property in the given content list
+     */
+    public static function max(array $contents, string $property)
+    {
+        return max(static::getAttributes($contents, $property));
+    }
+
+    /**
+     * Get min value of the given property in the given content list
+     */
+    public static function min(array $contents, string $property)
+    {
+        return min(static::getAttributes($contents, $property));
+    }
+
+    /**
+     * List all values for given property in the given list of contents
+     */
+    public static function getAttributes(array $contents, string $property): array
+    {
+        return array_map(
+            fn ($content) => static::getPropertyAccessor()->getValue($content, $property),
+            $contents
+        );
+    }
+
+    private static function getPropertyAccessor(): PropertyAccessorInterface
+    {
+        if (!static::$propertyAccessor) {
+            static::$propertyAccessor = PropertyAccess::createPropertyAccessor();
+        }
+
+        return static::$propertyAccessor;
+    }
+}
